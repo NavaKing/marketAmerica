527388ae843af2914543d30f74ba78b0953de421
NavaKing <ngiovannie@outlook.com>
2022-07-13 11:57:55 -0400

    added stylized address section

--- App.js
+++ App.js
@@ -3,9 +3,22 @@ import React from "react"
 import { ItemScreen } from "./src/features/items/screens/item.screen";
 import { theme } from "./src/infrastructure/theme"
 import { ThemeProvider } from "styled-components/native";
-
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [ladoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if(!oswaldLoaded || !ladoLoaded) {
+    return null
+  }
 
   return (
     <>
--- src/features/items/components/item-info-card.component.js
+++ src/features/items/components/item-info-card.component.js
@@ -1,28 +1,38 @@
 import React from "react";
-import styled from "styled-components/native"
-import { Text, StyleSheet,} from "react-native";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 const Title = styled(Text)`
-font-family: ${(props) => props.theme.fonts.heading};
- padding: ${(props) => props.theme.space[3]};
- color: ${(props)=> props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 const ItemCard = styled(Card)`
- background-color: ${(props) => props.theme.colors.bg.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const ItemCardCover = styled(Card.Cover)`
- padding: ${(props) => props.theme.space[3]};
- background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+
+const Address = styled.Text`
+font-family: ${(props) => props.theme.fonts.body};
+font-size: ${(props) => props.theme.fontSizes.caption};
+background-color: ${(props) => props.theme.colors.bg.primary};
+`
+
+const Info = styled.View `
+padding: ${(props) => props.theme.space[3]};
+`
 
 export const ItemInfoCard = ({ item = {} }) => {
   const {
     name = "Some item",
     icon,
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = " 100 some radom street",
     isOpenNow = true,
@@ -31,10 +41,12 @@ export const ItemInfoCard = ({ item = {} }) => {
   } = item;
 
   return (
-    <ItemCard elevation={5} >
-      <ItemCardCover key={name} source={{ uri: photos[0]}} />
+    <ItemCard elevation={5}>
+      <ItemCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
       <Title>{name}</Title>
+      <Address>{address}</Address>
+      </Info>
     </ItemCard>
   );
 };
-
